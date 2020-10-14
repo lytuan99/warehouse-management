8299dab613877da4577ea3638057e9f72cce0372
lytuan99 <lytuan270899@gmail.com>
2020-10-14 11:18:10 +0700

    start project

---
+++ projectI-warehouse/src/test/java/com/lytuan99/projectIwarehouse/ProjectIWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lytuan99.projectIwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
