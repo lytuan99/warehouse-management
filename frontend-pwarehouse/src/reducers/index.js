74cca2b11f49ef9296a621845c2f2cdd69cdb1e1
@pre_intern_2020_01_tuanlb <lytuan270899@gmail.com>
2021-01-02 22:23:05 +0700

    submit frontend

---
+++ frontend-pwarehouse/src/reducers/index.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux';
+import uiReducer from './ui';
+import modalReducer from './modal';
+import { reducer as formReducer } from 'redux-form';
+import { connectRouter } from 'connected-react-router';
+import receiptReducer from './receipt';
+import NavigationReducer from './NavigationReducer';
+import supplierReducer from './supplier';
+
+const rootReducer = (history) =>
+  combineReducers({
+    ui: uiReducer,
+    modal: modalReducer,
+    receipt: receiptReducer,
+    supplier: supplierReducer,
+    form: formReducer,
+    router: connectRouter(history),
+    navigations: NavigationReducer,
+  });
+export default rootReducer;
--- projectI-warehouse/src/main/java/com/lytuan99/projectIwarehouse/util/PathResource.java
+++ projectI-warehouse/src/main/java/com/lytuan99/projectIwarehouse/util/PathResource.java
@@ -1,7 +1,7 @@
 package com.lytuan99.projectIwarehouse.util;
 
 public class PathResource {
-   public static final String root = "E:/Lytuan20192/Project Sapo/FRONTEND/warehouse-management-font-end/public/image/";
+   public static final String root = "H:/20201_LyTuan/project-I/frontend-pwarehouse/public/image";
    public static final String path = "image/";
 }
 
